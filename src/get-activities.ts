b690cf603d070064fc5d08e218e41ad30711d2dc
Stephy Sam <stephysam008@gmail.com>
2026-01-19 15:59:24 +0530

    fix

--- src/get-activities.ts
+++ src/get-activities.ts
@@ -722,13 +722,13 @@ function activitiesFromPullRequests(
         link: pullRequest.url,
         points: null,
         meta: {
-          created_at: pullRequest.created_at,
-          merged_at: pullRequest.merged_at,
-          pr_number: pullRequest.number,
-          repo: repo,
+          pr_avg_tat:
+            new Date(pullRequest.merged_at).getTime() -
+            new Date(pullRequest.created_at).getTime(),
         },
       });
     }
+
     // PR review events
     for (const review of pullRequest.reviews) {
       if (!review.author) {
